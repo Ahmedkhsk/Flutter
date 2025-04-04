3c9dedf6ab58b507d40c768fff5c67205e401440
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-04-04 17:44:27 +0200

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -43,75 +43,74 @@ class MyProject extends StatelessWidget {
         child: const Icon(Icons.add, size: 40),
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        // mainAxisSize: MainAxisSize.max,
-        // crossAxisAlignment: CrossAxisAlignment.start,
-    
-        children: [
-          Container(
-            //color: const Color.fromARGB(255, 252, 220, 210),
-            margin: const EdgeInsets.all(55),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.max,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              //color: const Color.fromARGB(255, 252, 220, 210),
+              margin: const EdgeInsets.all(55),
 
-            //EdgeInsets.fromLTRB(22, 22, 15, 17),
-            padding: //EdgeInsets.fromLTRB(22, 5, 3, 6),
-                EdgeInsets.all(11),
+              //EdgeInsets.fromLTRB(22, 22, 15, 17),
+              padding: //EdgeInsets.fromLTRB(22, 5, 3, 6),
+                  EdgeInsets.all(11),
 
-            height: 280,
-            width: 280,
+              height: 280,
+              width: 280,
 
-            alignment: Alignment.center,
+              alignment: Alignment.center,
 
-            //transform: Matrix4.rotationZ(.02),
-            decoration: BoxDecoration(
-              //borderRadius: BorderRadius.circular(60),
-              shape: BoxShape.circle,
+              //transform: Matrix4.rotationZ(.02),
+              decoration: BoxDecoration(
+                //borderRadius: BorderRadius.circular(60),
+                shape: BoxShape.circle,
 
-              /*
+                /*
            shape
            borderRadius
             لو عملت الاتنين في نفس الوقت هيديك ايرور  
           */
-              color: const Color.fromARGB(255, 212, 188, 188),
-              // border: Border.all(
-              //   color: const Color.fromARGB(255, 63, 164, 240),
-              //   width: 10,
-              // ),
-            ),
-            child: const Text(
-              "Ahmed Khaled",
-              style: TextStyle(
-                fontSize: 25,
-                color: Color.fromARGB(255, 63, 164, 240),
-                fontWeight: FontWeight.bold,
-                fontFamily: "Cairo",
-                letterSpacing: 4,
-                wordSpacing: 2,
-                height: 1.5,
+                color: const Color.fromARGB(255, 212, 188, 188),
+                // border: Border.all(
+                //   color: const Color.fromARGB(255, 63, 164, 240),
+                //   width: 10,
+                // ),
+              ),
+              child: const Text(
+                "Ahmed Khaled",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Color.fromARGB(255, 63, 164, 240),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Cairo",
+                  letterSpacing: 4,
+                  wordSpacing: 2,
+                  height: 1.5,
+                ),
               ),
             ),
-          ),
-          
-          const SizedBox(
-            height: 20,
-          ),
 
-          Text(
-            "Shawky",
-            style: TextStyle(backgroundColor: Colors.tealAccent, fontSize: 22),
-          ),
+            const SizedBox(width: 200),
 
-          const SizedBox(
-            height: 20,
-          ),
+            Text(
+              "Shawky",
+              style: TextStyle(
+                backgroundColor: Colors.tealAccent,
+                fontSize: 22,
+              ),
+            ),
 
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.favorite,color: Colors.red, size: 99,),
-          ),
-        
-        ],
-      
+            const SizedBox(width: 200),
+
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.favorite, color: Colors.red, size: 99),
+            ),
+          ],
+        ),
       ),
     );
   }
